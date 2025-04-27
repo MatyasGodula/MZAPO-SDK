3bce4272414c48ce485596ef7fc0beacb4ce8200
matyas <matyasgodula@gmail.com>
2025-04-27 21:18:42 +0200

    Add docs for AudioDriver class and change the worker thread a bit.

--- AudioDriver.cpp
+++ AudioDriver.cpp
@@ -22,16 +22,21 @@ AudioDriver::AudioDriver() :
         throw std::runtime_error("Failed to map physical address");
     }
 
+    // Start the worker thread and provide it with our controled stop token.
     worker = std::jthread(
+        // Capture the current instance and pass the stop token to the lambda.
+        // A little note: This is actually very Rust-like.
         [this](std::stop_token token) {
             audio_thread_loop(token);
         },
+        // Pass our own stop token that is used in the destructor.
         stop_source.get_token()
     );
 }
 
-AudioDriver::~AudioDriver() noexcept {
+AudioDriver::~AudioDriver() {
     stop_source.request_stop();
+    // Added this line ^^ to try and prevent a potential deadlock.
     condvar.notify_one();
 }
 
@@ -51,51 +56,72 @@ void AudioDriver::turn_off_buzzer() {
     *(volatile uint32_t *)(pwm_reg + AUDIOPWM_REG_CR_o) = 0; // Disable
 }
 
+/// @note I tried to get as close to a Rust implementation as possible. 
 void AudioDriver::audio_thread_loop(std::stop_token stop_token) {
+    Tone tone = Tone::NullTone;
+    int duration_ms = 0;
     while (!stop_token.stop_requested()) {
-        std::unique_lock lock(current_tone_mutex);
-        condvar.wait(lock, [this, &stop_token]() { 
-            return new_tone_requested || stop_token.stop_requested(); 
-        });
+        // Uses a more Rust-like approach to handling mutexes.
+        // TODO: Test and debug this.
+        {
+            // Lock the mutex for the condvar.
+            std::unique_lock lock(current_tone_mutex);
+            // Condvar waits for a notification and checks the stop token and new tone flag.
+            condvar.wait(lock, [this, &stop_token]() { 
+                return new_tone_requested || stop_token.stop_requested(); 
+            });
 
-        if (stop_token.stop_requested()) {
-            break;
+            // If the stop token has been rquested, break out of the loop.
+            if (stop_token.stop_requested()) {
+                break;
+            }
+
+            // Copy important data to local variables.
+            tone = current_tone;
+            duration_ms = current_duration_ms;
+            // Reset the new tone flag.
+            new_tone_requested = false;
+        // End of the lock guard, mutex is automatically released here.
         }
 
-        Tone tone = current_tone;
-        int duration_ms = current_duration_ms;
-        new_tone_requested = false;
-        lock.unlock();
-
+        // Check if the tone is null or has a frequency of 0.
         if (tone.get_frequency() <= 0) {
             turn_off_buzzer();
             continue;
         }
 
-        
+        // Set the tone on the buzzer.
         set_tone(tone.get_frequency(), tone.get_duty());
 
+        // Start the timer for the duration of the tone.
         auto start_time = std::chrono::steady_clock::now();
+        // Periodically check the timer so we know when to stop the tone.
         while (std::chrono::steady_clock::now() - start_time < std::chrono::milliseconds(duration_ms)) {
+            // Check the stop token on every iteration.
             if (stop_token.stop_requested()) {
                 break;
             }
-            std::this_thread::sleep_for(std::chrono::milliseconds(5)); // Sleep a little, allow for early exit
+            // No need for perpetual checking.
+            // TODO: Evaluate the sleep time.
+            std::this_thread::sleep_for(std::chrono::milliseconds(5));
         }
-
+        // After the duration, turn off the buzzer.
         turn_off_buzzer();
     }
+    // Stop requested, end the thread.
     std::cout << "Stop requested, exiting audio thread..." << std::endl;
     turn_off_buzzer();
 }
 
 void AudioDriver::play_tone(Tone tone, int duration_ms) {
     {
+        // Lock the mutex, use lock_guard because we don't wait for condvars.
         std::lock_guard lock(current_tone_mutex);
         current_tone = tone;
         current_duration_ms = duration_ms;
         new_tone_requested = true;
     }
+    // Notify the worker thread.
     condvar.notify_one();
 }
 
--- AudioDriver.hpp
+++ AudioDriver.hpp
@@ -1,4 +1,7 @@
-/// @note There might be a deadlock somewhere in the code but idk where
+/// @file AudioDriver.hpp
+/// @brief Audio driver for the MZ-APO board
+/// @author Matyas Godula
+/// @date 26.4.2023
 
 #pragma once
 
@@ -12,6 +15,7 @@
 #include <stop_token>
 #include <thread>
 
+/// @brief A class representing a tone to be played.
 struct Tone {
 public: 
     constexpr Tone(uint32_t frequency, uint32_t duty)
@@ -35,34 +39,74 @@ private:
     uint32_t duty;
 };
 
+/// @brief Constants for the audio driver.
+/// @note This is a place to centralize sounds for the game.
 constexpr Tone Tone::PlayerMissile = Tone(440000, 1000);
 constexpr Tone Tone::EnemyMissile = Tone(300000, 1000);
 constexpr Tone Tone::Explosion = Tone(1000000, 1000);
 constexpr Tone Tone::NullTone = Tone(500000, 1000);
 
+
+/// @brief Audio driver for the PWM on the MZ-APO board.
+/// @note This class uses a worker thread to not block the main thread.
+/// @note The worker thread is stopped when the destructor is called.
 class AudioDriver {
 private:
-    void* pwm_reg;
-    std::jthread worker; // The worker thread repsonsible for sound playing
-    std::mutex current_tone_mutex; // Mutex to protect the current_tone
-    std::condition_variable condvar; // Condition variable to notify the worker thread
-    std::stop_source stop_source; // Stop source stored manually to manually stop the worker
+    /// @brief Pointer to the PWM register.
+    /// @note This is a pointer to the mapped physical address of the PWM register.
+    void* pwm_reg;  
+
+    /// @brief A worker thread responsible for playing sounds.
+    /// @note This thread is stopped when the destructor is called.
+    std::jthread worker;
+
+    /// @brief Mutex to protect the current_tone variable.
+    std::mutex current_tone_mutex;
+
+    /// @brief Condition variable to notify the worker thread when a new tone is requested.
+    std::condition_variable condvar;
+
+    /// @brief Stop source for the worker thread.
+    /// @note It is used to prevent deadlocks when the destructor is called.
+    // TODO: Test whether this actually works.
+    std::stop_source stop_source;
 
     Tone current_tone = Tone::NullTone;
     int current_duration_ms = 0;
     std::atomic<bool> new_tone_requested = false;
 
+    /// @brief Sets tthe tone on the buzzer according to the given data.
+    /// @param frequency 
+    /// @param duty 
+    /// @note This method is not thread safe, DO NOT CALL FROM THE MAIN THREAD!!!
     void set_tone(uint32_t frequency, uint32_t duty);
 
+    /// @brief Same as set_tone, but sets everything to 0.
     void turn_off_buzzer();
 
-public:
-    AudioDriver();
-    ~AudioDriver();
-
-    void play_tone(Tone tone, int duration_ms);
+    /// @brief The main loop of the worker thread.
+    /// @param stop_token This token is used by the jthread to stop the thread.
+    /// @note This method is called on the start of the worker thread.
     void audio_thread_loop(std::stop_token stop_token);
 
+public:
+    /// @brief Constructor for the AudioDriver class.
+    AudioDriver();
+
+    /// @brief Destructor for the AudioDriver class.
+    /// @note This destructor first sends a stop request and then wakes it up if needed.
+    /// @note It waits for the join before returning.
+    ~AudioDriver();
+
+    /// @brief Method to give a tone to the worker thread.
+    /// @param tone The tone to play.
+    /// @param duration_ms The duration of the tone in milliseconds.
+    /// @note This method is thread safe and can be called from the main thread.
+    void play_tone(Tone tone, int duration_ms);
+
+    /// @brief Turns off the buzzer.
+    /// @note This method does not stop the worker thread.
+    /// @note This method is thread safe and can be called from the main thread.
     void stop_tone();
 
 };
